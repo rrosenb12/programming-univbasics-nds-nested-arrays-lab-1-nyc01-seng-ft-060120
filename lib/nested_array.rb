715d6afd6eb89dbf52c0249f0e1534c2425f3d6c
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-05-04 19:34:30 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -71,7 +71,7 @@ def matrix_lookup(matrix, row, column)
     "Avocadoes",
     "Asparagus"]
   ]
-  produce_storag[row][column]
+  produce_storage[row][column]
 end
 
 def matrix_update(matrix, row, column, new_value)
